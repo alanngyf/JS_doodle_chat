3c6ae81cecea14049129adc4a0d8df2cbb0bfbbe
Frank Sun <popsun2010@gmail.com>
2015-10-01 20:09:55 -0700

    Fixed socket.io bug

--- server/config/socketIo.js
+++ server/config/socketIo.js
@@ -19,17 +19,17 @@ module.exports = function(io)
 		});
 		
 		//user login;
-	    socket.on('login', function(nickname) {
-	        if (users.indexOf(nickname) > -1) {
-	            socket.emit('nickExisted');
-	        } else {
-	            socket.userIndex = users.length;
-	            socket.nickname = nickname;
-	            users.push(nickname);
-	            socket.emit('loginSuccess');
-	            io.sockets.emit('system', nickname, users.length, 'login');
-	        };
-	    });
+	    // socket.on('login', function(nickname) {
+	    //     if (users.indexOf(nickname) > -1) {
+	    //         socket.emit('nickExisted');
+	    //     } else {
+	    //         socket.userIndex = users.length;
+	    //         socket.nickname = nickname;
+	    //         users.push(nickname);
+	    //         socket.emit('loginSuccess');
+	    //         io.sockets.emit('system', nickname, users.length, 'login');
+	    //     };
+	    // });
 
 	    //user leaves
 	    socket.on('disconnect', function() {
