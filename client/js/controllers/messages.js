0479f613cbe54da2fe7d25458644fcb5ae812abc
Frank Sun <popsun2010@gmail.com>
2015-10-01 19:55:04 -0700

    Start adding emoji featrue to chat room

--- client/js/controllers/messages.js
+++ client/js/controllers/messages.js
@@ -8,9 +8,18 @@ myApp.controller('MessageController', function($scope, MessagesFactory){
 			$scope.customers = data;
 		})
 	}
+	$scope.limit = 4;
 	MessagesFactory.get_history(function(data)
 	{
 		// console.log(data);
 		$scope.messages = data;
 	});
+	$scope.showMore = function()
+	{
+		$scope.limit += 4;
+		if($scope.messages.length < $scope.limit)
+		{
+			$scope.limit = $scope.messages.length;
+		}
+	}
 });
--- server/config/socketIo.js
+++ server/config/socketIo.js
@@ -9,11 +9,6 @@ module.exports = function(io)
 		    socket.broadcast.emit("drawling_response", data);
 		    // socket.emit('server_response', {response: "sockets are the best!"});
 		})	
-		// socket.on("drawling_req_end", function (data){
-		//     console.log(data);
-		//     // socket.emit('server_response', {response: "sockets are the best!"});
-		// })
-
 
 
 	//******************message board************************
@@ -21,6 +16,31 @@ module.exports = function(io)
 		{
 			doodlesController.save_message(data);
 			io.emit("get_msg", data);
-		})
+		});
+		
+		//user login;
+	    socket.on('login', function(nickname) {
+	        if (users.indexOf(nickname) > -1) {
+	            socket.emit('nickExisted');
+	        } else {
+	            socket.userIndex = users.length;
+	            socket.nickname = nickname;
+	            users.push(nickname);
+	            socket.emit('loginSuccess');
+	            io.sockets.emit('system', nickname, users.length, 'login');
+	        };
+	    });
+
+	    //user leaves
+	    socket.on('disconnect', function() {
+	        users.splice(socket.userIndex, 1);
+	        socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+	    });
+
+	    //new image get
+	    socket.on('img', function(imgData, color) {
+	        socket.broadcast.emit('newImg', socket.nickname, imgData, color);
+	    });
 	});
+
 }
